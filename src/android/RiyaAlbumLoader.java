f665134e0198d5408753a513879e07f0910ed35c
~Riya~ <riya.singh@adhocteam.com>
2023-08-07 13:51:46 +0530

    Update RiyaAlbumLoader.java

--- src/android/RiyaAlbumLoader.java
+++ src/android/RiyaAlbumLoader.java
@@ -3,37 +3,55 @@ package com.example;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-import java.io.File;
 import android.os.Environment;
+import java.io.File;
+import java.util.ArrayList;
+import org.json.JSONObject;
 
 public class RiyaAlbumLoader extends CordovaPlugin {
 
+    private static final String[] TARGET_FOLDERS = {"camera", "whatsapp", "instagram", "facebook", "DCIM"};
+
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("loadAlbums")) {
+            this.loadAlbums(callbackContext);
+            return true;
+        } else if (action.equals("loadPicturesInAlbum")) {
+            if(data != null && data.length() > 0){
+                String albumName = data.getString(0);
+                this.loadPicturesInAlbum(albumName, callbackContext);
+                return true;
+            }
+        }
 
-        if (action.equals("getAlbums")) {
-            String[] folders = {"Camera", "WhatsApp", "Instagram", "Facebook", "DCIM", "Pictures"};
+        return false;
+    }
 
-            JSONArray albums = new JSONArray();
-            
-            String state = Environment.getExternalStorageState();
-            if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-                // We can read the media
-                File root = Environment.getExternalStorageDirectory();
+    private void loadAlbums(CallbackContext callbackContext) {
+        // Logic to find folders containing images.
+    }
 
-                for (String folderName : folders) {
-                    File folder = new File(root, folderName);
-                    if (folder.isDirectory() && folder.listFiles().length > 0) {
-                        albums.put(folderName);
+    private void loadPicturesInAlbum(String albumName, CallbackContext callbackContext) {
+        try {
+            File picDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), albumName);
+
+            if (picDir.exists() && picDir.isDirectory()) {
+                ArrayList<String> pictures = new ArrayList<String>();
+                File[] allContents = picDir.listFiles();
+                for (File file : allContents) {
+                    if (file.isFile() && (file.getName().endsWith(".jpg") || file.getName().endsWith(".png"))) {
+                        pictures.add(file.getAbsolutePath());
                     }
                 }
-            }
 
-            // Send a positive result to the callbackContext
-            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, albums);
-            callbackContext.sendPluginResult(pluginResult);
-            return true;
+                JSONArray result = new JSONArray(pictures);
+                callbackContext.success(result);
+            } else {
+                callbackContext.error("Album does not exist or is not a directory.");
+            }
+        } catch (Exception e) {
+            callbackContext.error("Error loading pictures: " + e.getMessage());
         }
-        return false;  // Returning false results in a "MethodNotFound" error.
     }
 }
