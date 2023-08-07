148526b4547d56ddf39a2b6efeaaacfddeb8e1b5
~Riya~ <riya.singh@adhocteam.com>
2023-08-07 13:45:45 +0530

    Update RiyaAlbumLoader.java

--- src/android/RiyaAlbumLoader.java
+++ src/android/RiyaAlbumLoader.java
@@ -3,6 +3,8 @@ package com.example;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import java.io.File;
+import android.os.Environment;
 
 public class RiyaAlbumLoader extends CordovaPlugin {
 
@@ -10,10 +12,25 @@ public class RiyaAlbumLoader extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("getAlbums")) {
-            // TODO: implement your logic to get album list here
+            String[] folders = {"Camera", "WhatsApp", "Instagram", "Facebook", "DCIM"};
+
+            JSONArray albums = new JSONArray();
             
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+                // We can read the media
+                File root = Environment.getExternalStorageDirectory();
+
+                for (String folderName : folders) {
+                    File folder = new File(root, folderName);
+                    if (folder.isDirectory() && folder.listFiles().length > 0) {
+                        albums.put(folderName);
+                    }
+                }
+            }
+
             // Send a positive result to the callbackContext
-            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "Success");
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, albums);
             callbackContext.sendPluginResult(pluginResult);
             return true;
         }
