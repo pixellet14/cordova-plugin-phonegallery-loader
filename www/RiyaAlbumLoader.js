786b5c373b1c5448dae3c41f2c7e96d25e8c9dc6
~Riya~ <riya.singh@adhocteam.com>
2023-10-07 15:25:23 +0530

    Update RiyaAlbumLoader.js

--- www/RiyaAlbumLoader.js
+++ www/RiyaAlbumLoader.js
@@ -1,9 +1,12 @@
 var exec = require('cordova/exec');
 
-exports.loadAlbums = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'RiyaAlbumLoader', 'loadAlbums', []);
+var RiyaAlbumLoader = {
+    loadAlbums: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "RiyaAlbumLoader", "loadAlbums", []);
+    },
+    loadPicturesInAlbum: function(albumName, startIndex, count, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "RiyaAlbumLoader", "loadPicturesInAlbum", [albumName, startIndex, count]);
+    }
 };
 
-exports.loadPicturesInAlbum = function(albumName, startIndex, count, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'RiyaAlbumLoader', 'loadPicturesInAlbum', [albumName, startIndex, count]);
-};
+module.exports = RiyaAlbumLoader;
